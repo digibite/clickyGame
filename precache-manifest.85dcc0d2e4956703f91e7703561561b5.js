b29bf9217ddb40ed57c96846472f8b2471767521
irwing gameros <irwinggameros@irwings-MacBook-Pro.local>
2019-02-05 09:24:29 -0800

    Updates

---
+++ precache-manifest.85dcc0d2e4956703f91e7703561561b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1a9c84edd9e07832fe40",
+    "url": "/clickyGame/static/css/main.833c2b47.chunk.css"
+  },
+  {
+    "revision": "1a9c84edd9e07832fe40",
+    "url": "/clickyGame/static/js/main.1a9c84ed.chunk.js"
+  },
+  {
+    "revision": "9c49b1657f794805e3dd",
+    "url": "/clickyGame/static/js/1.9c49b165.chunk.js"
+  },
+  {
+    "revision": "f5e1124212e913e930db",
+    "url": "/clickyGame/static/js/runtime~main.f5e11242.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/clickyGame/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ccc5026075c8fdfa56a9e580b79895b9",
+    "url": "/clickyGame/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ffef9c2c02a9431342a3e54b84f14f6e.js"
+  "/clickyGame/precache-manifest.85dcc0d2e4956703f91e7703561561b5.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/clickyGame/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
